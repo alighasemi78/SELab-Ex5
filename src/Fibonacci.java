4e352a52c96699f854ffb834e1a039bf92fb9f17
hesanghasemi <hesanghasemi99@gmail.com>
2021-11-16 22:26:22 +0330

    after optimization

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -8,9 +8,12 @@ public class Fibonacci {
     }
 
     private static int findFib(int n) {
-        if (n <= 1) {
-            return n;
+        int f[] = new int[n + 2];
+        f[0] = 0;
+        f[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            f[i] = f[i - 1] + f[i - 2];
         }
-        return findFib(n - 1) + findFib(n - 2);
+        return f[n];
     }
 }
